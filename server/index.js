90fcedf26d7f5434bc74079be6206117d58e1258
Dyslex7c <polynsominiac@gmail.com>
2024-11-01 13:49:05 +0530

    chore: removed multer

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-import multer from "multer";
 import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
@@ -34,15 +33,6 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors(corOpts));
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
-
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, "public/assets");
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.originalname);
-    }
-});
 
 app.post("/auth/register", register)
 
